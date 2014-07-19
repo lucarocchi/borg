82619dd0808e23006a08091006f6ff23b51e8b3d
Luca Rocchi <lr1313@gmail.com>
2014-07-19 12:48:21 +0200

    Initial Commit

---
+++ Borg/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Borg
+//
+//  Created by Luca Rocchi on 19/07/14.
+//  Copyright (c) 2014 mobileborg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
